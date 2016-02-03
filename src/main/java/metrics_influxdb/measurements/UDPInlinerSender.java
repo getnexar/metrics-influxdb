23cb61bd00b26318f41bd712d47195735b394cea
roiaddi <roiaddi@getnexar.com>
2016-02-03 11:27:27 +0200

    up line by line

--- src/main/java/metrics_influxdb/measurements/UDPInlinerSender.java
+++ src/main/java/metrics_influxdb/measurements/UDPInlinerSender.java
@@ -27,25 +27,27 @@ public class UDPInlinerSender extends QueueableSender {
 
     @Override
     protected boolean doSend(Collection<Measurement> measures) {
+        boolean returnValue = true;
         if (measures.isEmpty()) {
             return true;
         }
-        
-        DatagramChannel channel = null;
+        for (Measurement measure: measures) {
+          DatagramChannel channel = null;
 
-		String measuresAsString = inliner.inline(measures);
-		try {
-			if (LOGGER.isDebugEnabled()) {
-            	LOGGER.debug("measurements being sent:\n{}", measuresAsString);
+          String measuresAsString = inliner.inline(measure);
+          try {
+            if (LOGGER.isDebugEnabled()) {
+              LOGGER.debug("measurements being sent:\n{}", measuresAsString);
             }
-			channel = DatagramChannel.open();
-			ByteBuffer buffer = ByteBuffer.wrap(measuresAsString.getBytes(Miscellaneous.UTF8));
-			channel.send(buffer, serverAddress);
-			LOGGER.debug("{} measurements sent to UDP[{}:{}]", measures.size(), serverAddress.getHostString(), serverAddress.getPort());
-			return true;
-		} catch (IOException e) {
-			LOGGER.info("failed to send {} mesures to UDP[{}:{}], {}", measures.size(), serverAddress.getHostString(), serverAddress.getPort(), e.getMessage(), e);
-			return false;
-		}
+            channel = DatagramChannel.open();
+            ByteBuffer buffer = ByteBuffer.wrap(measuresAsString.getBytes(Miscellaneous.UTF8));
+            channel.send(buffer, serverAddress);
+            LOGGER.debug("{} measurements sent to UDP[{}:{}]", measures.size(), serverAddress.getHostString(), serverAddress.getPort());
+          } catch (IOException e) {
+            LOGGER.info("failed to send {} mesures to UDP[{}:{}], {}", measures.size(), serverAddress.getHostString(), serverAddress.getPort(), e.getMessage(), e);
+            returnValue = false;
+          }
+        }
+      return returnValue;
     }
 }
